4815302d00abd6b45d1642c9dcb844fb74728f31
jianghailong.rd <jianghailong.rd@gmail.com>
2026-08-28 10:10:13 +0200

    feat(outcomes): add constrained action executor

--- src/apiserver/src/outcome-reconciler/outcome-reconciler.module.ts
+++ src/apiserver/src/outcome-reconciler/outcome-reconciler.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OutcomeEvaluatorService } from './outcome-evaluator.service';
 import { OutcomeFactIngressService } from './outcome-fact-ingress.service';
+import { ActionCapabilityRegistry, ActionExecutorService } from './action-executor.service';
 
 @Module({
-  providers: [OutcomeFactIngressService, OutcomeEvaluatorService],
-  exports: [OutcomeFactIngressService, OutcomeEvaluatorService],
+  providers: [
+    OutcomeFactIngressService,
+    OutcomeEvaluatorService,
+    ActionCapabilityRegistry,
+    ActionExecutorService,
+  ],
+  exports: [
+    OutcomeFactIngressService,
+    OutcomeEvaluatorService,
+    ActionCapabilityRegistry,
+    ActionExecutorService,
+  ],
 })
 export class OutcomeReconcilerModule {}
