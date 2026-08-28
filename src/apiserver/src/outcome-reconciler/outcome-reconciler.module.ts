800fea1dfbd15999b0f48abf644495e29c5eed16
jianghailong.rd <jianghailong.rd@gmail.com>
2026-08-28 09:17:59 +0200

    feat(outcomes): add rebuildable obligation projection

--- src/apiserver/src/outcome-reconciler/outcome-reconciler.module.ts
+++ src/apiserver/src/outcome-reconciler/outcome-reconciler.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OutcomeEvaluatorService } from './outcome-evaluator.service';
 import { OutcomeFactIngressService } from './outcome-fact-ingress.service';
+import { OutcomeProjectionService } from './outcome-projection.service';
 
 @Module({
-  providers: [OutcomeFactIngressService, OutcomeEvaluatorService],
-  exports: [OutcomeFactIngressService, OutcomeEvaluatorService],
+  providers: [OutcomeFactIngressService, OutcomeEvaluatorService, OutcomeProjectionService],
+  exports: [OutcomeFactIngressService, OutcomeEvaluatorService, OutcomeProjectionService],
 })
 export class OutcomeReconcilerModule {}
